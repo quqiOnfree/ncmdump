9f6d2f8800a1784e78b55d671d7cc0f192622ab4
quqiOnfree <quqiOnfree@outlook.com>
2025-03-02 10:35:00 +0800

    Reverse 'decrypting AES data with multi-threads'

--- src/ncmcrypt.cpp
+++ src/ncmcrypt.cpp
@@ -3,7 +3,6 @@
 #include "base64.h"
 #include "cJSON.h"
 #include "color.h"
-#include "thread_pool.hpp"
 
 #define TAGLIB_STATIC
 #include "taglib/toolkit/tfile.h"
@@ -16,7 +15,6 @@
 #include <stdexcept>
 #include <string>
 #include <filesystem>
-#include <vector>
 
 #pragma warning(disable:4267)
 #pragma warning(disable:4244)
@@ -28,8 +26,6 @@ const unsigned char NeteaseCrypt::mPng[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A,
 
 static void aesEcbDecrypt(const unsigned char *key, std::string &src, std::string &dst)
 {
-    cxxpool::ThreadPool thread_pool;
-    thread_pool.start();
     int n = src.length() >> 4, i;
     dst.clear();
     dst.resize(n << 4);
@@ -37,11 +33,8 @@ static void aesEcbDecrypt(const unsigned char *key, std::string &src, std::strin
 
     for (i = 0; i < n - 1; i++)
     {
-        thread_pool.submit(&AES::decrypt, &aes, (const unsigned char *)src.c_str() + (i << 4), (unsigned char *)dst.data() + (i << 4));
+        aes.decrypt((const unsigned char *)src.c_str() + (i << 4), (unsigned char *)dst.data() + (i << 4));
     }
-
-    if (thread_pool.joinable())
-        thread_pool.join();
 
     aes.decrypt((const unsigned char *)src.c_str() + (i << 4), (unsigned char *)dst.data() + (i << 4));
     char pad = dst.c_str()[(i << 4) + 15];
